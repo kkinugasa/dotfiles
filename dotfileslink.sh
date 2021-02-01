bf08607ee725fdf937414acb702ee7a2d4d6fb24
Kimiaki Kinugasa <kimiakikinugasa@gmail.com>
2021-02-01 22:25:47 +0900

    chore: add git commit template

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -3,3 +3,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.profile ~/.profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
+git config --global commit.template ~/.gitmessage.txt
