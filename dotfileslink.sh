b2dff32fe61df9a7947a89d57311273e519afd2a
kkinugasa <kimiakikinugasa@gmail.com>
2023-05-01 00:24:15 +0900

    refactor: minor revision

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.profile ~/.profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
+ln -sf $(pwd)/.vimrc ~/.vimrc
+ln -sf $(pwd)/.profile ~/.profile
+ln -sf $(pwd)/.bashrc ~/.bashrc
+ln -sf $(pwd)/.gitmessage.txt ~/.gitmessage.txt
 git config --global commit.template ~/.gitmessage.txt
