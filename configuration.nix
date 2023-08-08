be41404262c946d9d3832f5ca748ad525cec7502
kkinugasa <kimiakikinugasa@gmail.com>
2023-08-08 21:26:46 +0900

    add packages

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    nvtop
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wev
     wget
--- home.nix
+++ home.nix
@@ -1,8 +1,33 @@
+{ pkgs, ... }:
+
 {
   home = rec {
     username="kkinugasa";
     homeDirectory = "/home/${username}";
     stateVersion = "23.05";
   };
+  home.packages = with pkgs; [
+    bat # cat alternative
+    bottom # top alternative
+    discord
+    du-dust # du  alternative
+    duf # df alternative
+    exa # ls alternative
+    fd # find alternative
+    gh
+    ghq
+    go
+    google-chrome
+    httpie # curl alternative
+    lean
+    peco
+    pinta # Microsoft Paint alternative
+    poetry
+    pyenv
+    ripgrep # grep alternative
+    slack
+    unzip
+    vscode
+  ];
   programs.home-manager.enable = true;
 }
