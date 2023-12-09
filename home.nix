be2301d470aef12883932c2a8ca4edf2cbf53984
kkinugasa <kimiakikinugasa@gmail.com>
2023-12-09 11:27:12 +0900

    update nix flake

--- home.nix
+++ home.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
-let
-    mypkgs = import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
-      sha256 = "sha256:1r6c7ggdk0546wzf2hvd5a7jwzsf3gn1flr8vjd685rm74syxv6d";
-    }) {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-    };
-    vscode = mypkgs.vscode;
-in
+# let
+#     mypkgs = import (builtins.fetchTarball {
+#       url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
+#       sha256 = "sha256:1r6c7ggdk0546wzf2hvd5a7jwzsf3gn1flr8vjd685rm74syxv6d";
+#     }) {
+#       system = "x86_64-linux";
+#       config.allowUnfree = true;
+#     };
+#     vscode = mypkgs.vscode;
+# in
 {
   home = rec {
     username="kkinugasa";
@@ -38,7 +38,9 @@ in
     slack
     texlive.combined.scheme-full
     unzip
-  ] ++ [vscode];
+    vscode
+  ];
+  # ++ [vscode];
 
   programs.bash = {
     enable = true;
