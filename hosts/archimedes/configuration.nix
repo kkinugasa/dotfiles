599cce77740bb1968cb690f2601b4d8be0207e65
kkinugasa <kimiakikinugasa@gmail.com>
2025-07-25 03:12:44 +0900

    remove duplicated nixpkgs config

--- hosts/archimedes/configuration.nix
+++ hosts/archimedes/configuration.nix
@@ -150,9 +150,6 @@
   };
   virtualisation.docker.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
